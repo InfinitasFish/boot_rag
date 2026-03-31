c18da0a866f44779bf13ee918595a7a22190e737
InfinitasFish <iq000012@gmail.com>
2026-03-31 15:25:44 +0500

    tf-idf 1

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,8 +1,11 @@
 import json
 import pickle
 import os
+from collections import Counter
+# import sys
+# sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
-from consts import MOVIES_JSON_PATH, INDEX_DB_PATH, DOCMAP_PATH
+from consts import MOVIES_JSON_PATH, INDEX_DB_PATH, DOCMAP_PATH, TERM_FREQ_PATH
 from preprocess import preprocess_text_to_tokens_pipe
 
 
@@ -13,19 +16,27 @@ class InvertedIndex:
     def __init__(self):
         self.index = {}
         self.docmap = {}
+        self.term_frequencies = {}
 
     def __add_document(self, doc_id, text):
         text_tokens = preprocess_text_to_tokens_pipe(text)
-        for token in text_tokens:
+        self.term_frequencies[doc_id] = Counter(text_tokens)
+        for token in self.term_frequencies[doc_id]:
             if token in self.index:
-                self.index[token].append(doc_id)
+                self.index[token].add(doc_id)
             else:
-                self.index[token] = [doc_id]
+                self.index[token] = set([doc_id])
     
     def get_documents(self, term):
         term = term.lower()
         idx = sorted(self.index[term])
         return idx
+    
+    def get_tf(self, doc_id, term):
+        token = preprocess_text_to_tokens_pipe(term)
+        if len(token) != 1:
+            raise ValueError("InvertedIndex.get_tf() accepts only one term\n")
+        return self.term_frequencies[doc_id][token[0]]
 
     def build(self, movies_path=MOVIES_JSON_PATH):
         with open(MOVIES_JSON_PATH, 'r') as f:
@@ -36,24 +47,39 @@ class InvertedIndex:
             self.__add_document(movie["id"], movie_text)
             self.docmap[movie["id"]] = movie
 
-    # saves .index and .docmap to disk
-    def save(self, idx_save_path=INDEX_DB_PATH, docmap_save_path=DOCMAP_PATH):
+    # saves .index, .docmap and .term_frequencies to disk
+    def save(self, idx_save_path=INDEX_DB_PATH, docmap_save_path=DOCMAP_PATH, tf_save_path=TERM_FREQ_PATH):
         idx_dir_path = os.path.abspath(os.path.dirname(idx_save_path))
         if not os.path.exists(idx_dir_path):
             os.mkdir(idx_dir_path)
+        if os.path.exists(idx_save_path):
+            os.remove(idx_save_path)
 
         docmap_dir_path = os.path.abspath(os.path.dirname(docmap_save_path))
         if not os.path.exists(docmap_dir_path):
             os.mkdir(docmap_dir_path)
+        if os.path.exists(docmap_save_path):
+            os.remove(docmap_save_path)
+
+        tf_dir_path = os.path.abspath(os.path.dirname(tf_save_path))
+        if not os.path.exists(tf_dir_path):
+            os.mkdir(tf_dir_path)
+        if os.path.exists(tf_save_path):
+            os.remove(tf_save_path)
 
         pickle.dump(self.index, open(idx_save_path, "wb"))
         pickle.dump(self.docmap, open(docmap_save_path, "wb"))
+        pickle.dump(self.term_frequencies, open(tf_save_path, "wb"))
 
-    def load(self, idx_load_path=INDEX_DB_PATH, docmap_load_path=DOCMAP_PATH):
+    def load(self, idx_load_path=INDEX_DB_PATH, docmap_load_path=DOCMAP_PATH, tf_load_path=TERM_FREQ_PATH):
         if not os.path.exists(idx_load_path):
             raise ValueError(f"Unexisting path to index DB: {idx_load_path}\n")
         if not os.path.exists(docmap_load_path):
             raise ValueError(f"Unexisting path to docmap: {docmap_load_path}\n")
-        
+        if not os.path.exists(tf_load_path):
+            raise ValueError(f"Unexisting path to term frequencies: {tf_load_path}\n")
+
         self.index = pickle.load(open(idx_load_path, "rb"))
         self.docmap = pickle.load(open(docmap_load_path, "rb"))
+        self.term_frequencies = pickle.load(open(tf_load_path, "rb"))
+
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -4,6 +4,7 @@ import json
 import string
 import argparse
 import os
+from math import log
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
@@ -19,6 +20,12 @@ search_parser.add_argument("query", type=str, help="Search query")
 
 build_parser = subparsers.add_parser("build", help="Build Index DB for movies")
 
+term_freq_parser = subparsers.add_parser("tf", help="Get frequency of term in movie")
+term_freq_parser.add_argument("movie_id", type=int, help="Movie id")
+term_freq_parser.add_argument("term", type=str, help="Term to get frequency of in movie")
+
+idf_parser = subparsers.add_parser("idf", help="Get inverse document frequency score for a term")
+idf_parser.add_argument("term", type=str, help="Term to get idf score of")
 
 # adapting new "" quotes for strings instead of '' from now on
 def main() -> None:
@@ -41,6 +48,22 @@ def main() -> None:
             for i, ids in enumerate(found_movies_ids):
                 if i >= 5: break
                 print(f"{i+1}. {idx_db.docmap[ids]['title']} {ids}")
+        case "tf":
+            idx_db.load()
+            if args.movie_id in idx_db.docmap:
+                print(f"Movie: {idx_db.docmap[args.movie_id]['title']}; Term-freq: {args.term} {idx_db.get_tf(args.movie_id, args.term)}")
+            else:
+                print(f"Unknown movie_id {args.movie_id}")
+        case "idf":
+            idx_db.load()
+            term_token = preprocess_text_to_tokens_pipe(args.term)
+            if len(term_token) != 1:
+                raise ValueError("Idf accepts only one term\n")
+
+            # avoid zero-division
+            appearance_count = len(idx_db.index.get(term_token[0], []))
+            term_idf_score = log((len(idx_db.docmap) + 1) / (appearance_count + 1))
+            print(f"Inverse document frequency score of '{args.term}': {term_idf_score:.2f}")
         case _:
             parser.print_help()
 
